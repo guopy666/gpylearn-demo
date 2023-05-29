97c2bab122c07e76161f245211a51b937b8d8fb0
guopy <1315977102@qq.com>
2023-05-29 15:17:03 +0800

    spring全家桶跟练-aop

--- springalltong/src/main/java/com/gpy/springalltong/aopdemo/AopDemoMain.java
+++ springalltong/src/main/java/com/gpy/springalltong/aopdemo/AopDemoMain.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
